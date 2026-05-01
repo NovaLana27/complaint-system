59f37c18a4f1382e2f023d36b5e98d7faf0a176b
estherkojusola98-crypto <estherkojusola98@gmail.com>
2026-05-01 12:10:58 -0700

    added empty test skeletons

---
+++ tests/test_app.py
@@ -0,0 +1,27 @@
+# Test 1 - Student Registration
+def test_student_registration():
+    pass
+
+# Test 2 - Student Login
+
+
+def test_student_login():
+    pass
+
+# Test 3 - Submit a Complaint
+
+
+def test_submit_complaint():
+    pass
+
+# Test 4 - Block Edit After Status Change
+
+
+def test_block_edit_after_status_change():
+    pass
+
+# Test 5 - Admin Status Update
+
+
+def test_admin_status_update():
+    pass
